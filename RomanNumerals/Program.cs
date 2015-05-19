f75090ace64f8cc707ebdfd23dbfca0591f3c413
Matteo Manzo <imatteo.manzo@gmail.com>
2015-05-19 13:56:34 +0100

    program working

---
+++ RomanNumerals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RomanNumerals
+{
+	public class Program
+	{
+		public static void Main ()
+		{
+			Console.WriteLine ("Type a number");
+			string input = Console.ReadLine ();
+			int num = Convert.ToInt32 (input);
+			Console.WriteLine (NumberConverter.Convert(num));
+		}
+	}
+}
+
